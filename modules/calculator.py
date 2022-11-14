Now the calculator works, still some bugs as the test on real cases does not reproduce the force as derivative of the energy. Other issue to fix is the acoustic sum rule

modules/calculator.py
@@ -4,6 +4,7 @@ from ase.calculators.calculator import Calculator
 import cellconstructor as CC
 import cellconstructor.Structure
 import cellconstructor.Methods
+import cellconstructor.Phonons
 
 import numpy as np
 import sys, os
@@ -35,6 +36,9 @@ class ElectrostaticCalculator(Calculator):
         self.reciprocal_vectors = None
         self.cutoff = 5 # Stop the sum when k > 5/ eta
         self.kpoints = None
+
+
+        self.implemented_properties = ["energy", "forces"]#, "stress"]
 
     def init(self, reference_structure : CC.Structure.Structure, effective_charges : np.ndarray, dielectric_tensor : np.ndarray, supercell : tuple[int, int, int] = (1,1,1)) -> None:
         """
@@ -80,6 +84,9 @@ class ElectrostaticCalculator(Calculator):
         self.reciprocal_vectors = CC.Methods.get_reciprocal_vectors(self.reference_structure.unit_cell)
 
 
+        self.init_kpoints()
+
+
     def init_kpoints(self):
         r"""
         INITIALIZE THE K POINTS
@@ -100,14 +107,15 @@ class ElectrostaticCalculator(Calculator):
         
         self.kpoints = []
 
-        for l in range(-max_values, max_values + 1):
-            for m in range(-max_values, max_values+1):
-                for n in range(-max_values, max_values+1):
+        for l in range(-max_values[0], max_values[0] + 1):
+            for m in range(-max_values[1], max_values[1] + 1):
+                for n in range(-max_values[2], max_values[2] + 1):
                     kvector = l * self.reciprocal_vectors[0, :] 
                     kvector += m * self.reciprocal_vectors[1, :] 
                     kvector += n * self.reciprocal_vectors[2, :] 
 
-                    if np.linalg.norm(kvector) < self.cutoff / self.eta:
+                    knorm = np.linalg.norm(kvector)
+                    if knorm < self.cutoff / self.eta and knorm > 1e-6:
                          self.kpoints.append(kvector)
         
         self.kpoints = np.array(self.kpoints)
@@ -134,24 +142,52 @@ class ElectrostaticCalculator(Calculator):
         self.init(dynamical_matrix.structure, dynamical_matrix.effective_charges, dynamical_matrix.dielectric_tensor, dynamical_matrix.GetSupercell())
 
 
-    def _get_energy_force(self, struct : CC.Structure.Structure):
+
+    def check_asr(self, threshold = 1e-6):
         """
-        helper function to evaluate the energy
+        Check if the acoustic sum rule is enforced on the effective charges.
+        This is very important to properly compute the forces on the structure.
         """
 
+        nat = self.reference_structure.N_atoms
+        for i in range(3):
+            for j in range(3):
+                asr_thr = np.sum(self.effective_charges[:, i, j])
+
+                if asr_thr > threshold:
+                    raise ValueError("Error, atom index {}, electric field {} does not satisfy the ASR by {}".format(j, i, asr_thr))
+
+        v = np.random.uniform(size = 3)
+        total_shift = np.zeros(3)
+        for i in range(nat):
+            total_shift += self.effective_charges[i, :, :].dot(v)
+
+        print("total shift:", total_shift)
+        if np.linalg.norm(total_shift) > threshold:
+            raise ValueError("Error, a translation is giving problems")
+
+    def _get_energy_force(self, struct):
         n_atoms = self.reference_structure.N_atoms
 
         delta_r = struct.coords - self.reference_structure.coords  
+        delta_r *= CC.Units.A_TO_BOHR
 
         n_kpoints = self.kpoints.shape[0]
         volume = struct.get_volume() * CC.Units.A_TO_BOHR
 
-        energy = 0
+        energy = 0 + 0j
         force = np.zeros_like(struct.coords)
 
+        #print("Energy calculation:")
+        #print("-------------------")
 
         for kindex in range(n_kpoints):
             kvect = self.kpoints[kindex, :]
+
+            # Discard Gamma
+            if np.linalg.norm(kvect) < 1e-6:
+                continue
+
             k2 = kvect.dot(kvect)
             
             k_eps_k = kvect.dot(self.dielectric_tensor .dot(kvect))
@@ -159,17 +195,42 @@ class ElectrostaticCalculator(Calculator):
 
             #ZkkZ = np.einsum("ai, bj, ab -> ij", self.work_charges, self.work_charges, kk_matrix)
             ZkkZ = self.work_charges.T.dot(kk_matrix.dot(self.work_charges))
+            
+            #print()
+            #print("k point: ", kvect)
+            #print("ZkkZ:", ZkkZ)
+
 
             for i in range(n_atoms):
                 for j in range(n_atoms):
-                    exp_factor = np.exp(-1j* kvect.dot(struct.coords[j, :] - struct.coords[i, :]))
-                    cos_factor = exp_factor + np.conj(exp_factor)
-                    sin_factor = 1j * (exp_factor - np.conj(exp_factor))
-                    energy -= delta_r[i, :].dot(ZkkZ.dot(delta_r[j, :])) * exp_factor 
+                    delta_rij = struct.coords[j, :] - struct.coords[i, :]
+                    delta_rij *= CC.Units.A_TO_BOHR
 
-                    force[i, :] +=  ZkkZ.dot(delta_r[j, :]) * cos_factor
-                    force[i, :] -= delta_r[i, :] * ZkkZ.dot(delta_r[j, :])  * kvect *  sin_factor
 
+                    exp_factor = np.exp(-1j* kvect.dot(delta_rij))
+                    cos_factor = np.real(exp_factor + np.conj(exp_factor))
+                    sin_factor = np.real(1j * (exp_factor - np.conj(exp_factor)))
+
+                    ZkkZr = ZkkZ[3*i:3*i+3, 3*j:3*j+3].dot(delta_r[j, :])
+
+                    energy -= delta_r[i, :].dot(ZkkZr) * exp_factor 
+
+                    #print("Energy k:",  delta_r[i, :].dot(ZkkZr) * exp_factor )
+                    #print("Force k:", ZkkZr * cos_factor + delta_r[i, :] * ZkkZr  * kvect *  sin_factor)
+
+                    if np.isnan(energy):
+                        print("Error, energy is NaN")
+                        print("i: {};  j: {}".format(i, j))
+                        print("exp: ", exp_factor)
+                        print("k: ", kvect)
+                        print("delta R_ij: ", struct.coords[j, :] - struct.coords[i, :])
+                        raise ValueError("Error, energy is NaN")
+
+                    force[i, :] +=  ZkkZr * cos_factor
+                    force[i, :] += delta_r[i, :] * ZkkZr  * kvect *  sin_factor
+
+        assert np.imag(energy) < 1e-6, "Error, the energy has an imaginary part: {}".format(energy)
+        energy = np.real(energy)
 
         self.energy = energy * 4 * np.pi / volume * CC.Units.HA_TO_EV
         self.force = force *  4 * np.pi / volume * CC.Units.HA_TO_EV / CC.Units.BOHR_TO_ANGSTROM
@@ -193,7 +254,3 @@ class ElectrostaticCalculator(Calculator):
         #self.results["dipole"] = self.get_dipole() 
 
 
-
-
-        
-        

tests/BaTiO3/test_unit_cell.py
@@ -8,6 +8,88 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def test_1D(plot = False):
+    s = CC.Structure.Structure(1)
+    s.unit_cell = np.eye(3)
+    s.has_unit_cell = True
+
+    effective_charges = np.zeros((1,3,3), dtype = np.double)
+    dielectric_tensor = np.eye(3)
+
+    effective_charges[0, :, :] = np.eye(3)
+
+
+    calculator = calc.ElectrostaticCalculator()
+    calculator.eta = 4
+    calculator.init(s.copy(), effective_charges, dielectric_tensor)    
+    print("kpts:", calculator.kpoints)
+
+    n_steps = 20
+    dstep = 0.1
+
+    energy = np.zeros(n_steps, dtype = np.double)
+    forces = np.zeros((n_steps, 1, 3), dtype = np.double)
+    x_values = np.zeros_like(energy)
+
+    for i in range(n_steps):
+        s.coords[0,0] += dstep
+        atm = s.get_ase_atoms()
+        atm.set_calculator(calculator)
+        energy[i] = atm.get_total_energy()
+        forces[i, :, :] = atm.get_forces()
+        x_values[i] = s.coords[0,0]
+
+    if plot:
+        plt.figure()
+        plt.title("1 particle")
+        plt.plot(x_values, -np.gradient(energy, x_values), label = "Numerical diff")
+        plt.plot(x_values, forces[:, 0,0], label = "Forces")
+        plt.legend()
+        plt.tight_layout()
+
+def test_2D(plot = False):
+    s = CC.Structure.Structure(2)
+    s.unit_cell = np.eye(3)
+    s.has_unit_cell = True
+    s.coords[1, :] = .5
+
+    effective_charges = np.zeros((2,3,3), dtype = np.double)
+    dielectric_tensor = np.eye(3)
+
+    effective_charges[0, :, :] = np.eye(3)
+    effective_charges[1, :, :] = -np.eye(3)
+
+
+    calculator = calc.ElectrostaticCalculator()
+    calculator.eta = 1
+    calculator.init(s.copy(), effective_charges, dielectric_tensor)    
+    print("kpts:", calculator.kpoints)
+
+    n_steps = 20
+    dstep = 0.1
+
+    energy = np.zeros(n_steps, dtype = np.double)
+    forces = np.zeros((n_steps, 2, 3), dtype = np.double)
+    x_values = np.zeros_like(energy)
+
+    for i in range(n_steps):
+        s.coords[0,0] += dstep
+        atm = s.get_ase_atoms()
+        atm.set_calculator(calculator)
+        energy[i] = atm.get_total_energy()
+        forces[i, :, :] = atm.get_forces()
+        x_values[i] = s.coords[0,0]
+
+    if plot:
+        plt.figure()
+        plt.title("2 particles")
+        plt.plot(x_values, -np.gradient(energy, x_values), label = "Numerical diff")
+        plt.plot(x_values, forces[:, 0,0], label = "Forces")
+        plt.legend()
+        plt.tight_layout()
+
+
+
 def test_total_translation(plot = False):
     np.random.seed(0)
 
@@ -16,14 +98,16 @@ def test_total_translation(plot = False):
     os.chdir(total_path)
 
     BaTiO3 = CC.Phonons.Phonons("BaTiO3_")
+    BaTiO3.Symmetrize()
     nat = BaTiO3.structure.N_atoms
 
-    #for i in range(BaTiO3.structure.N_atoms):
-    #    BaTiO3.effective_charges[i, :, :] = np.eye(3) * np.trace(BaTiO3.effective_charges[i, :, :]) / 3
+    for i in range(BaTiO3.structure.N_atoms):
+        BaTiO3.effective_charges[i, :, :] = np.eye(3) #* np.trace(BaTiO3.effective_charges[i, :, :]) / 3
 
-    calculator = calc.LongRangeInteractions()
-    calculator.init_from_dyn(BaTiO3)
-    #calculator.dielectric_tensor[:,:] = np.eye(3)
+    calculator = calc.ElectrostaticCalculator()
+    calculator.init_from_phonons(BaTiO3)
+    calculator.check_asr()
+    calculator.dielectric_tensor[:,:] = np.eye(3)
 
     struct = BaTiO3.structure.copy()
     struct.coords += np.random.normal(size = struct.coords.shape, scale = 0.05)
@@ -34,13 +118,9 @@ def test_total_translation(plot = False):
     N_steps  = 50
 
     energies = np.zeros(N_steps, dtype = np.double)
-    electric_field = np.zeros((N_steps, 3), dtype = np.double)
-    de_dr = np.zeros((N_steps, 3), dtype = np.double)
     forces = np.zeros(N_steps, dtype = np.double)
 
     xvalues = np.zeros(N_steps, dtype = np.double)
-    s_charges = np.zeros( (N_steps, 3 * nat, 3), dtype = np.double)
-    s_force = np.zeros( (N_steps, 3 * nat, 3), dtype = np.double)
     asr = np.zeros(N_steps, dtype = np.double)
     asr2 = np.zeros(N_steps, dtype = np.double)
     ss = []
@@ -61,10 +141,13 @@ def test_total_translation(plot = False):
 
         asr[i] = np.linalg.norm( np.sum(atm.get_forces(), axis = 0))
 
-        electric_field[i,:] = calculator.get_electric_field(r_value, verbose = True)
-        de_dr[i, :] = np.sum(calculator.get_derivative_efield(r_value)[:, direction, :])
+        # Check the product of the position vector with the effective charges
+        asr2[i] = np.linalg.norm( calculator.work_charges.dot(struct.coords.ravel()))
 
-        asr2[i] = np.linalg.norm( np.sum(calculator.u_disps, axis =0))
+        #electric_field[i,:] = calculator.get_electric_field(r_value, verbose = True)
+        #de_dr[i, :] = np.sum(calculator.get_derivative_efield(r_value)[:, direction, :])
+
+        #asr2[i] = np.linalg.norm( np.sum(calculator.u_disps, axis =0))
 
         #print("CHARGES:")
         #print(calculator.charges)
@@ -78,13 +161,8 @@ def test_total_translation(plot = False):
         plt.legend()
         plt.tight_layout()
         plt.figure()
-        plt.plot(xvalues, np.gradient(electric_field[:,0], delta), label = "Numerical efield diff")
-        plt.plot(xvalues, de_dr[:, 0], label = "Anal efield diff") 
-        plt.legend()
-        plt.tight_layout()
-        plt.figure()
-        plt.plot(xvalues, electric_field[:,0], label = "E field")
-        #plt.plot(xvalues, asr2, label = "asr on displacements")
+        plt.plot(xvalues, asr, label = "ASR")
+        plt.plot(xvalues, asr2, label = "ASR - 2")
         plt.legend()
         plt.tight_layout()
         #ase.visualize.view(ss)
@@ -111,8 +189,8 @@ def test_unit_cell(plot = False):
     for i in range(BaTiO3.structure.N_atoms):
         BaTiO3.effective_charges[i, :, :] = np.eye(3) * np.trace(BaTiO3.effective_charges[i, :, :]) / 3
 
-    calculator = calc.LongRangeInteractions()
-    calculator.init_from_dyn(BaTiO3)
+    calculator = calc.ElectrostaticCalculator()
+    calculator.init_from_phonons(BaTiO3)
     #calculator.dielectric_tensor[:,:] = np.eye(3)
 
     struct = BaTiO3.structure.copy()
@@ -125,15 +203,10 @@ def test_unit_cell(plot = False):
     N_steps  = 50
 
     energies = np.zeros(N_steps, dtype = np.double)
-    electric_field = np.zeros((N_steps, 3), dtype = np.double)
-    de_dr = np.zeros((N_steps, 3), dtype = np.double)
     forces = np.zeros(N_steps, dtype = np.double)
 
     xvalues = np.zeros(N_steps, dtype = np.double)
-    s_charges = np.zeros( (N_steps, 3 * nat, 3), dtype = np.double)
-    s_force = np.zeros( (N_steps, 3 * nat, 3), dtype = np.double)
     asr = np.zeros(N_steps, dtype = np.double)
-    asr2 = np.zeros(N_steps, dtype = np.double)
     ss = []
 
     for i in range(N_steps):    
@@ -149,10 +222,6 @@ def test_unit_cell(plot = False):
 
         asr[i] = np.linalg.norm( np.sum(atm.get_forces(), axis = 0))
 
-        electric_field[i,:] = calculator.get_electric_field(np.zeros(3))
-        de_dr[i, :] = calculator.get_derivative_efield(np.zeros(3))[atm_id, direction, :]
-
-        asr2[i] = np.linalg.norm( np.sum(calculator.u_disps, axis =0))
 
         
     if plot:
@@ -163,13 +232,7 @@ def test_unit_cell(plot = False):
         plt.legend()
         plt.tight_layout()
         plt.figure()
-        plt.plot(xvalues, np.gradient(electric_field[:,0], delta), label = "Numerical efield diff")
-        plt.plot(xvalues, de_dr[:, 0], label = "Anal efield diff") 
-        plt.legend()
-        plt.tight_layout()
-        plt.figure()
         plt.plot(xvalues, asr, label = "asr")
-        plt.plot(xvalues, asr2, label = "asr on displacements")
         plt.legend()
         plt.tight_layout()
         #ase.visualize.view(ss)
@@ -187,8 +250,8 @@ def test_one_atom_model(plot = False):
     dyn.dielectric_tensor = np.eye(3)
     nat = 1
     
-    calculator = calc.LongRangeInteractions()
-    calculator.init_from_dyn(dyn)
+    calculator = calc.ElectrostaticCalculator()
+    calculator.init_from_phonons(dyn)
     #calculator.dielectric_tensor[:,:] = np.eye(3)
 
     struct = dyn.structure.copy()
@@ -204,13 +267,9 @@ def test_one_atom_model(plot = False):
     e_field_point[1] = 1
 
     energies = np.zeros(N_steps, dtype = np.double)
-    electric_field = np.zeros((N_steps, 3), dtype = np.double)
-    de_dr = np.zeros((N_steps, 3), dtype = np.double)
     forces = np.zeros(N_steps, dtype = np.double)
 
     xvalues = np.zeros(N_steps, dtype = np.double)
-    s_charges = np.zeros( (N_steps, 3 * nat, 3), dtype = np.double)
-    s_force = np.zeros( (N_steps, 3 * nat, 3), dtype = np.double)
 
     ss = []
 
@@ -226,12 +285,6 @@ def test_one_atom_model(plot = False):
         forces[i] = atm.get_forces()[atm_id, direction]
 
         r_mean = np.mean(calculator.charge_coords, axis = 0)
-        electric_field[i,:] = calculator.get_electric_field(r_mean)
-        de_dr[i, :] = calculator.get_derivative_efield(r_mean)[atm_id, direction, :]
-
-        print("CHARGES:")
-        print(calculator.charges)
-        print(calculator.charge_coords)
 
     #assert np.max(np.abs(forces[1:-1] + np.gradient(energies, delta)[1:-1])) < 1e-6
 
@@ -243,18 +296,11 @@ def test_one_atom_model(plot = False):
         plt.plot(xvalues, forces, label = "Forces")
         plt.legend()
         plt.tight_layout()
-        plt.figure()
-        plt.plot(xvalues, np.gradient(electric_field[:,0], delta), label = "Numerical efield diff")
-        plt.plot(xvalues, de_dr[:, 0], label = "Anal efield diff") 
-        plt.legend()
-        plt.tight_layout()
-        plt.figure()
-        plt.plot(xvalues, electric_field[:,0], label = "E field")
-        plt.legend()
-        plt.tight_layout()
 
 if __name__ == "__main__":
+    #test_1D(True)
+    test_2D(True)
     #test_total_translation(True)
-    test_unit_cell(plot = True)
+    #test_unit_cell(plot = True)
     #test_one_atom_model(plot = True)
     plt.show()